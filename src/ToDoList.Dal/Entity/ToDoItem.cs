a40421ba0e832f1386422d9bba2550bb99323e3d
iislommm <ibragimegamberdiyev81@gmail.com>
2025-04-12 11:48:56 +0500

    entities added

--- src/ToDoList.Dal/Entity/ToDoItem.cs
+++ src/ToDoList.Dal/Entity/ToDoItem.cs
@@ -2,5 +2,10 @@
 
 public class ToDoItem
 {
-
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsComplited { get; set; } = false;
 }
