8923f4b04f4d5ce21ffb974b0b48491dc5b9095f
Your Name <you@example.com>
2025-04-12 12:09:38 +0500

    Dto

---
+++ src/ToDoList.Service/Dtos/ToDoItemUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Service.Dtos;
+
+public class ToDoItemUpdateDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsComplited { get; set; }
+}
