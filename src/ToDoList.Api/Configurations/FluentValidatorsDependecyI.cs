04bdf384e842ab3a4c5507a561d647d8c641b209
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-04-12 14:36:59 +0500

    di fluent validators

---
+++ src/ToDoList.Api/Configurations/FluentValidatorsDependecyI.cs
@@ -0,0 +1,12 @@
+﻿using ToDoList.Service.Validators;
+using FluentValidation;
+
+namespace ToDoList.Api.Configurations;
+
+public static class FluentValidatorsDependecyI
+{
+    public static void AddFluentValidators(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<ToDoItemCreateDtoValidator>();
+    }
+}
